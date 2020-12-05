94af8d6be1c403327442f16acb32ff055ca62867
Houcem Ben Amor Chlegou <nicolastsue@gmail.com>
2020-12-05 12:11:15 +0100

    add routing + 2 buttons (list + form)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MemberListComponent} from './member-list/member-list.component';
+import {MemberFormComponent} from './member-form/member-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'members',
+  },
+  {
+    path: 'members',
+    pathMatch: 'full',
+    component: MemberListComponent,
+  },
+  {
+    path: 'member-form',
+    pathMatch: 'full',
+    component: MemberFormComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'members',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
