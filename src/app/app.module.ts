89b578de772c621a508cca83057e22c6da0f4eb3
Houcem Ben Amor Chlegou <nicolastsue@gmail.com>
2020-12-19 12:40:15 +0100

    add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from '../@root/shared.module';
 import { MemberFormComponent } from './member-form/member-form.component';
+import {LayoutComponent} from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MemberListComponent,
-    MemberFormComponent
+    MemberFormComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
