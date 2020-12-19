9c8c9421aa3f95e22ba9740ed79aeeeb4720beff
Houcem Ben Amor Chlegou <nicolastsue@gmail.com>
2020-12-19 12:09:20 +0100

    integrate gateway endpoints in member service.

--- src/models/member.model.ts
+++ src/models/member.model.ts
@@ -1,7 +1,10 @@
+/*TODO: add missed properties*/
 export interface Member {
   id: string;
   cin: string;
   name: string;
+  nom: string;
+  prenom: string;
   createdDate: string;
   cv: string;
   type: string;
--- src/services/member.service.ts
+++ src/services/member.service.ts
@@ -2,11 +2,14 @@ import {Injectable} from '@angular/core';
 import {GLOBAL} from '../app/app-config';
 import {Member} from '../models/member.model';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
+  private path = `${environment.gatewayEndpoint}/membre-service`;
+  // @ts-ignore
   public placeholderMembers: Member[] = GLOBAL._DB.members;
 
   constructor(
@@ -15,15 +18,11 @@ export class MemberService {
   }
 
   getAllMembers(): Promise<Member[]> {
-    // return this.httpClient.get<Member[]>('linkToRestApi').toPromise();
-    return new Promise(resolve => resolve(this.placeholderMembers));
+    return this.httpClient.get<Member[]>(`${this.path}/membres`).toPromise();
   }
 
   getMemberById(id: string): Promise<Member> {
-    // return this.httpClient.get<Member>('linkToRestApi').toPromise();
-    return new Promise(resolve => resolve(
-      this.placeholderMembers.filter(item => item.id === id)[0] ?? null
-    ));
+    return this.httpClient.get<Member>(`${this.path}/membres/${id}`).toPromise();
   }
 
   /**
@@ -31,21 +30,26 @@ export class MemberService {
    * a new member doesn't have an id
    */
   saveMember(member: any): Promise<Member> {
-    // return this.httpClient.post<Member>('linkToRestApi', member).toPromise();
-    const memberToSave = {
-      id: member.id ?? Math.ceil(Math.random() * 10000).toString(),
-      createdDate: member.createdDate ?? new Date().toISOString(), ...member
-    };
-    this.placeholderMembers = [memberToSave, ...this.placeholderMembers.filter(item => item.id !== member.id)];
-
-    return new Promise(resolve => resolve(memberToSave));
+    if (!!member.id) {
+      return this.updateMember(member.id, member);
+    } else {
+      return this.createMember(member);
+    }
   }
+
+  createMember(member: any): Promise<Member> {
+    return this.httpClient.post<Member>(`${this.path}/membres/etudiant`, member).toPromise();
+  }
+
+  updateMember(id: string, member: any): Promise<Member> {
+    return this.httpClient.put<Member>(`${this.path}/membres/etudiant/${id}`, member).toPromise();
+  }
+
 
   removeMemberById(id: string): Promise<void> {
     // return this.httpClient.delete<void>('linkToRestApi').toPromise();
     this.placeholderMembers = this.placeholderMembers.filter(item => item.id !== id);
     return new Promise(resolve => resolve());
   }
-
 
 }
